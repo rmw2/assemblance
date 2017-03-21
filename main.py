0302d5349b7f21ffa83a715ffa84af474dbeba2f
Rob Whitaker <rmw2@princeton.edu>
2017-03-21 14:50:39 -0400

    began work on server-side compilation

--- main.py
+++ main.py
@@ -9,9 +9,17 @@ from werkzeug.utils import secure_filename
 # project files
 from parse import process_asm, format_c
 
+# standard packages
+import os, subprocess, elftools
+from uuid import uuid4
+
 # Intialize Application
 app = Flask(__name__)
 ALLOWED_EXTENSIONS = set(['c', 's', 'o'])
+UPLOADS_FOLDER = 'uploads'
+
+# Location of gcc on the server
+gcc = '/usr/local/gcc-4.8.1-for-linux64/bin/x86_64-pc-linux-gcc'
 
 # Secret key to maintain sessions
 app.secret_key = 'not so secret'
@@ -33,6 +41,8 @@ def index():
 
     Issue-- this currently re-renders the entire template every time...
     """
+    if 'uid' not in session:
+        session['uid'] = uuid4()
     if 'src-markup' not in session:
         session['src-markup'] = 'source goes here'
     if 'asm-markup' not in session:
@@ -47,19 +57,36 @@ def index():
             # Save text of source file to session object
             session['src'] = [line.decode('UTF-8') for line in file.readlines()]
             session['src-markup'] = format_c(session['src'])
-        elif 'asmfile' in request.files:
-            file = request.files['asmfile']
-            # Save assembly file to session
-            session['asm'] = [line.decode('UTF-8') for line in file.readlines()]
 
-            # Process assembly and generate markup
+            # Save text of source file to uploads folder
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(UPLOADS_FOLDER, session['uid'])
+            file.save(os.path.join(filepath, filename))
+
+            # Compile source to assembly
+            pres1 = subprocess.call([gcc, '-g', '-S', os.path.join(filepath, filename)])
+
+            # Compile source to ELF
+            pres2 = subprocess.call([gcc, '-g', '-c', os.path.join(filepath, filename)])
+
+            # Check for compilation errors
+            if pres1 != 0 or pres2 !=0:
+                flash('Compilation failed')
+                return redirect(request.url)
+
+            # Load assembly and ELF and process
+            with open(os.path.join(filepath, filename[:-2], '.s')) as sfile:
+                session['asm'] = [line.decode('UTF-8') for line in sfile.readlines()]
+            with open(os.path.join(filepath, filename[:-2], '.o')) as ofile:
+                session['elf'] = elftools.ELFFile(ofile)
+
+            # Process
             (session['asm-markup'], session['colors']) = process_asm(session['asm'])
         else:
             return redirect(request.url)
 
         if 'src' in session and 'asm' in session:
             session['src-markup'] = format_c(session['src'], session['colors'])
-
 
     return render_template('index.html', srctext=session['src-markup'], asmtext=session['asm-markup'])
 
