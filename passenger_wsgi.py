cb0f82e1b032b5d9093b52d66b8dbffa2b08fdd8
Rob Whitaker <rmw2@princeton.edu>
2017-11-08 23:50:48 -0500

    added passenger_wsgi

---
+++ passenger_wsgi.py
@@ -0,0 +1,14 @@
+import os, sys, pwd, grp
+
+INTERP = "/n/fs/asm/src/venv/bin/python"
+if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
+
+def get_username():
+    return pwd.getpwuid(os.getuid()).pw_name
+
+def get_groups():
+    return grp.getgrgid(os.getgid()).gr_name
+
+def application(env, start_response):
+    start_response('200 OK', [('Content-Type','text/html')])
+    return ["Hello World!"]
